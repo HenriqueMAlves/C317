d57ab5fbc305806049cf6ff67b369aeaa542288a
Henrique PD <henrique@pdsolucoes.com.br>
2022-05-20 14:13:18 -0300

    refactor: change user class

--- Inaclicker/Assets/Scripts/UserInfo.cs
+++ Inaclicker/Assets/Scripts/UserInfo.cs
@@ -7,22 +7,25 @@ namespace UnityEngine
     // Resumo:
     //     `Users` is a class that stores Player informations between game sessions.
     //     It can store string, float and integer values into the user’s platform registry.
-    public static class Users
-    {
-        public static List<UserInfo> users = new List<UserInfo>();
-    }
-    
     public class UserInfo
     {
-        public static string name { get; set; }
-        public static int score { get; set; }
-        public static int rank { get; set; }
+        public UserInfo() { }
+        public string name { get; set; }
+        public int score { get; set; }
+        public int rank { get; set; }
+        public int index { get; set; }
 
-        public UserInfo(string name, int score, int rank)
+        public UserInfo(string name, int score, int rank, int index)
         {
-            UserInfo.name = name;
-            UserInfo.score = score;
-            UserInfo.rank = rank;
+            this.name = name;
+            this.score = score;
+            this.rank = rank;
+            this.index = index;
         }
+    }
+
+    public class User
+    {
+        public static List<UserInfo> users = new List<UserInfo>();
     }
 }

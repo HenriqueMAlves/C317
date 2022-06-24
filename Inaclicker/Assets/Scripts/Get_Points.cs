5ad3c87c4e6daa297b07063159c0a4ffc22b6973
Henrique PD <henrique@pdsolucoes.com.br>
2022-06-24 08:29:51 -0300

    feat: add list of players

--- Inaclicker/Assets/Scripts/Get_Points.cs
+++ Inaclicker/Assets/Scripts/Get_Points.cs
@@ -8,18 +8,20 @@ public class Get_Points : MonoBehaviour
 {
     void Start()
     {
-        GameObject.Find("Pontos Num").GetComponent<Text>().text = User.users[0].score.ToString();
+        int playerId = PlayerPrefs.GetInt("actualPlayerId");
+        GameObject.Find("Pontos Num").GetComponent<Text>().text = User.users[playerId].score.ToString();
         //if(PlayerPrefs.GetInt("lastLevel") != 2)//Se veio da tela Game
         {
-            GameObject.Find("BackGround").GetComponent<Image>().sprite = User.users[0].background;
-            GameObject.Find("Table").GetComponent<Image>().sprite = User.users[0].table;
-            GameObject.Find("Wall").GetComponent<Image>().sprite = User.users[0].wall;
+            GameObject.Find("BackGround").GetComponent<Image>().sprite = User.users[playerId].background;
+            GameObject.Find("Table").GetComponent<Image>().sprite = User.users[playerId].table;
+            GameObject.Find("Wall").GetComponent<Image>().sprite = User.users[playerId].wall;
         }
     }
 
     public void countPoint()
     {
-        User.users[0].score++;
-        GameObject.Find("Pontos Num").GetComponent<Text>().text = User.users[0].score.ToString();
+        int playerId = PlayerPrefs.GetInt("actualPlayerId");
+        User.users[playerId].score++;
+        GameObject.Find("Pontos Num").GetComponent<Text>().text = User.users[playerId].score.ToString();
     }
 }
--- Inaclicker/Assets/Scripts/New_Player_Button.cs
+++ Inaclicker/Assets/Scripts/New_Player_Button.cs
@@ -22,14 +22,40 @@ public class New_Player_Button : MonoBehaviour
         playerName = GameObject.Find("Entrada Nome").GetComponent<InputField>().text;
         if (playerName.Length > 0)
         {
-            User.users.Add(new UserInfo(playerName, 0, 0, 0));
-            User.users[0].name = playerName;
-            User.users[0].score = 0;
-            User.users[0].rank = 0;
-            User.users[0].background = defaultBackground;
-            User.users[0].table = defaultTable;
-            User.users[0].wall = defaultWall;
-            SceneManager.LoadScene(nextScene);
+            int i = 0;
+            bool flagUser = false;
+            UserInfo newUser = new UserInfo(playerName, 0, 0, 0);
+
+            foreach (UserInfo user in User.users)
+            {
+                print(user.name);
+                //print("rank: " + user.rank);
+                print("score: " + user.score);
+                //print("wall: " + user.wall);
+                //print("table: " + user.table);
+                //print("backgeound: " + user.background);
+                print("user número: " + i);
+                if (newUser.name.Equals(user.name))
+                {
+                    print("Danada");
+                    print("numero: " + i);
+                    flagUser = true;
+                }
+                i++;
+            }
+            if(flagUser == false)
+            {
+                User.users.Add(newUser);
+                User.users[i].name = playerName;
+                User.users[i].score = 0;
+                User.users[i].rank = 0;
+                User.users[i].background = defaultBackground;
+                User.users[i].table = defaultTable;
+                User.users[i].wall = defaultWall;
+                PlayerPrefs.SetInt("actualPlayerId", i);
+                print("novo user número: " + i);
+                SceneManager.LoadScene(nextScene);
+            }
         }
     }
 }
